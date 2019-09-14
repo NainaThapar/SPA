56fd7ac0a4c73af05002b6d85fe29940d29e10ef
Naina Thapar <Naina@Nainas-MacBook-Pro-3.local>
2019-09-14 21:23:52 +0530

    adding index.js

--- To do list/app/index.js
+++ To do list/app/index.js
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+import Main from "../components/Main/main";
+
 class App extends React.Component {
   render() {
-    return <div>To Do List</div>;
+    return (
+      <div>
+        <h2>To Do List</h2>
+        <Main></Main>
+      </div>
+    );
   }
 }
 
